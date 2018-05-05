63df8b0f3213db42eb55c3673582e1ac645a090c
David Gutheinz <djgutheinz@outlook.com>
2018-05-05 16:20:18 -0500

    Update (Cloud) TP-Link TunableWhite Bulb Habitat.groovy

--- Device Handlers/(Cloud) TP-Link TunableWhite Bulb Habitat.groovy
+++ Device Handlers/(Cloud) TP-Link TunableWhite Bulb Habitat.groovy
@@ -1,4 +1,4 @@
-/*	TP Link Cloud TunableWhite Bulbs Device Driver, Hubitat Version 1
+/*	TP Link Cloud TP-Link TunableWhite Bulb Emon Device Driver, Hubitat Version 1
 	Copyright 2018 Dave Gutheinz
 
 Licensed under the Apache License, Version 2.0(the "License");
@@ -24,7 +24,7 @@ primarily various users on GitHub.com.
 ========================================================*/
 
 metadata {
-	definition (name: "(Cloud) TP-Link TunableWhite Bulb",
+	definition (name: "(Cloud) TP-Link TunableWhite Bulb Emon",
 				namespace: "davegut",
 				author: "Dave Gutheinz") {
 		capability "Switch"
@@ -34,8 +34,16 @@ metadata {
 		capability "Color Temperature"
 		command "setModeNormal"
 		command "setModeCircadian"
-        command "refresh"
 		attribute "bulbMode", "string"
+        command "refresh"
+        command "poll"
+		command "getPower"
+		capability "Energy Meter"
+		command "getEnergyStats"
+		attribute "monthTotalE", "string"
+		attribute "monthAvgE", "string"
+		attribute "weekTotalE", "string"
+		attribute "weekAvgE", "string"
 	}
 
 	def rates = [:]
@@ -82,33 +90,43 @@ def update() {
 		state.transTime = 1000 * lightTransTime
 	} else {
 		state.transTime = 5000
-    }
+	}
+	schedule("0 05 0 * * ?", setCurrentDate)
+	schedule("0 10 0 * * ?", getEnergyStats)
+	setCurrentDate()
 	runIn(2, refresh)
+	runIn(7, getEnergyStats)
 }
 
 void uninstalled() {
-	def alias = device.label
-	log.debug "Removing device ${alias} with DNI = ${device.deviceNetworkId}"
-	parent.removeChildDevice(alias, device.deviceNetworkId)
+	if (state.installType == "Cloud") {
+		def alias = device.label
+		log.debug "Removing device ${alias} with DNI = ${device.deviceNetworkId}"
+		parent.removeChildDevice(alias, device.deviceNetworkId)
+	}
 }
 
 //	===== Basic Bulb Control/Status =====
 def on() {
 	sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"on_off":1,"transition_period":${state.transTime}}}}""", "deviceCommand", "commandResponse")
+	runIn(2, getPower)
 }
 
 def off() {
 	sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"on_off":0,"transition_period":${state.transTime}}}}""", "deviceCommand", "commandResponse")
+	runIn(2, getPower)
 }
 
 def setLevel(percentage) {
 	percentage = percentage as int
 	sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"ignore_default":1,"on_off":1,"brightness":${percentage},"transition_period":${state.transTime}}}}""", "deviceCommand", "commandResponse")
+	runIn(2, getPower)
 }
 
 def setColorTemperature(kelvin) {
 	kelvin = kelvin as int
 	sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"ignore_default":1,"on_off":1,"color_temp": ${kelvin},"hue":0,"saturation":0}}}""", "deviceCommand", "commandResponse")
+	runIn(2, getPower)
 }
 
 def setModeNormal() {
@@ -117,21 +135,23 @@ def setModeNormal() {
 
 def setModeCircadian() {
 	sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"mode":"circadian"}}}""", "deviceCommand", "commandResponse")
+	runIn(2, getPower)
 }
 
-def poll() {
+def poll(){
 	sendCmdtoServer('{"system":{"get_sysinfo":{}}}', "deviceCommand", "commandResponse")
 }
 
 def refresh(){
 	sendCmdtoServer('{"system":{"get_sysinfo":{}}}', "deviceCommand", "commandResponse")
+	runIn(2, getPower)
 }
 
 def commandResponse(cmdResponse){
 	def status
-	def respType = cmdResponse.toString().substring(1,7)
-    if (respType == "smartl") {
-		status =  cmdResponse["smartlife.iot.smartbulb.lightingservice"]["transition_light_state"]
+	def respType = cmdResponse.toString().substring(0,9)
+    if (respType == "{smartlif") {
+		status = cmdResponse["smartlife.iot.smartbulb.lightingservice"]["transition_light_state"]
 	} else {
 		status = cmdResponse.system.get_sysinfo.light_state
 	}
@@ -152,7 +172,203 @@ def commandResponse(cmdResponse){
 	sendEvent(name: "colorTemperature", value: color_temp)
 }
 
-//	===== Send the Command =====
+//	===== Get Current Energy Data =====
+def getPower(){
+	sendCmdtoServer("""{"smartlife.iot.common.emeter":{"get_realtime":{}}}""", "deviceCommand", "energyMeterResponse")
+	runIn(5, getConsumption)
+}
+
+def energyMeterResponse(cmdResponse) {
+	def realtime = cmdResponse["smartlife.iot.common.emeter"]["get_realtime"]
+	if (realtime.power == null) {
+		state.powerScale = "power_mw"
+		state.energyScale = "energy_wh"
+	} else {
+		state.powerScale = "power"
+		state.energyScale = "energy"
+	}
+	def powerConsumption = realtime."${state.powerScale}"
+	if (state.powerScale == "power_mw") {
+		powerConsumption = Math.round(powerConsumption/10) / 100
+	} else {
+		powerConsumption = Math.round(100*powerConsumption) / 100
+	}
+	sendEvent(name: "power", value: powerConsumption)
+	log.info "$device.name $device.label: Updated CurrentPower to $powerConsumption"
+}
+
+//	===== Get Today's Consumption =====
+def getConsumption(){
+	sendCmdtoServer("""{"smartlife.iot.common.emeter":{"get_daystat":{"month": ${state.monthToday}, "year": ${state.yearToday}}}}""", "emeterCmd", "useTodayResponse")
+}
+
+def useTodayResponse(cmdResponse) {
+	def wattHrToday
+	def wattHrData
+	def dayList = cmdResponse["smartlife.iot.common.emeter"]["get_daystat"].day_list
+	for (int i = 0; i < dayList.size(); i++) {
+		wattHrData = dayList[i]
+		if(wattHrData.day == state.dayToday) {
+			wattHrToday = wattHrData."${state.energyScale}"
+ 		}
+	}
+	if (state.powerScale == "power") {
+		wattHrToday = Math.round(1000*wattHrToday)
+	}
+	sendEvent(name: "energy", value: wattHrToday)
+	log.info "$device.name $device.label: Updated Usage Today to ${wattHrToday}"
+}
+
+//	===== Get Weekly and Monthly Stats =====
+def getEnergyStats() {
+	state.monTotEnergy = 0
+	state.monTotDays = 0
+	state.wkTotEnergy = 0
+	state.wkTotDays = 0
+	sendCmdtoServer("""{"smartlife.iot.common.emeter":{"get_daystat":{"month": ${state.monthToday}, "year": ${state.yearToday}}}}""", "emeterCmd", "engrStatsResponse")
+	runIn(4, getPrevMonth)
+}
+
+def getPrevMonth() {
+	def prevMonth = state.monthStart
+	if (state.monthToday == state.monthStart) {
+		//	If all of the data is in this month, do not request previous month.
+		//	This will occur when the current month is 31 days.
+		return
+	} else if (state.monthToday - 2 == state.monthStart) {
+		//	If the start month is 2 less than current, we must handle
+		//	the data to get a third month - January.
+		state.handleFeb = "yes"
+		prevMonth = prevMonth + 1
+		runIn(4, getJan)
+	}
+	sendCmdtoServer("""{"smartlife.iot.common.emeter":{"get_daystat":{"month": ${prevMonth}, "year": ${state.yearStart}}}}""", "emeterCmd", "engrStatsResponse")
+}
+
+def getJan() {
+//	Gets January data on March 1 and 2.  Only accessed if current month = 3
+//	and start month = 1
+	sendCmdtoServer("""{"smartlife.iot.common.emeter":{"get_daystat":{"month": ${state.monthStart}, "year": ${state.yearStart}}}}""", "emeterCmd", "engrStatsResponse")
+}
+
+def engrStatsResponse(cmdResponse) {
+/*	
+	This method parses up to two energy status messages from the device,
+	adding the energy for the previous 30 days and week, ignoring the
+	current day.  It then calculates the 30 and 7 day average formatted
+	in kiloWattHours to two decimal places.
+*/
+	def dayList = cmdResponse["smartlife.iot.common.emeter"]["get_daystat"].day_list
+	if (!dayList[0]) {
+		log.info "$device.name $device.label: Month has no energy data."
+		return
+	}
+	def monTotEnergy = state.monTotEnergy
+	def wkTotEnergy = state.wkTotEnergy
+	def monTotDays = state.monTotDays
+	def wkTotDays = state.wkTotDays
+    def startDay = state.dayStart
+	def dataMonth = dayList[0].month
+	if (dataMonth == state.monthToday) {
+		for (int i = 0; i < dayList.size(); i++) {
+			def energyData = dayList[i]
+			monTotEnergy += energyData."${state.energyScale}"
+			monTotDays += 1
+			if (state.dayToday < 8 || energyData.day >= state.weekStart) {
+				wkTotEnergy += energyData."${state.energyScale}"
+				wkTotDays += 1
+			}
+			if(energyData.day == state.dayToday) {
+				monTotEnergy -= energyData."${state.energyScale}"
+				wkTotEnergy -= energyData."${state.energyScale}"
+				monTotDays -= 1
+				wkTotDays -= 1
+			}
+		}
+	} else if (state.handleFeb == "yes" && dataMonth == 2) {
+    	startDay = 1
+		for (int i = 0; i < dayList.size(); i++) {
+			def energyData = dayList[i]
+			if (energyData.day >= startDay) {
+				monTotEnergy += energyData."${state.energyScale}"
+				monTotDays += 1
+			}
+			if (energyData.day >= state.weekStart && state.dayToday < 8) {
+				wkTotEnergy += energyData."${state.energyScale}"
+				wkTotDays += 1
+			}
+		}
+	} else if (state.handleFeb == "yes" && dataMonth == 1) {
+		for (int i = 0; i < dayList.size(); i++) {
+			def energyData = dayList[i]
+			if (energyData.day >= startDay) {
+				monTotEnergy += energyData."${state.energyScale}"
+				monTotDays += 1
+			}
+			state.handleFeb = ""
+		}
+	} else {
+		for (int i = 0; i < dayList.size(); i++) {
+			def energyData = dayList[i]
+			if (energyData.day >= startDay) {
+				monTotEnergy += energyData."${state.energyScale}"
+				monTotDays += 1
+			}
+			if (energyData.day >= state.weekStart && state.dayToday < 8) {
+				wkTotEnergy += energyData."${state.energyScale}"
+				wkTotDays += 1
+			}
+		}
+	}
+	state.monTotDays = monTotDays
+	state.monTotEnergy = monTotEnergy
+	state.wkTotEnergy = wkTotEnergy
+	state.wkTotDays = wkTotDays
+	log.info "$device.name $device.label: Update 7 and 30 day energy consumption statistics"
+    if (monTotDays == 0) {
+    	//	Aviod divide by zero on 1st of month
+    	monTotDays = 1
+        wkTotDays = 1 
+	}
+	def monAvgEnergy =monTotEnergy/monTotDays
+	def wkAvgEnergy = wkTotEnergy/wkTotDays
+	if (state.powerScale == "power_mw") {
+		monAvgEnergy = Math.round(monAvgEnergy/10)/100
+		wkAvgEnergy = Math.round(wkAvgEnergy/10)/100
+		monTotEnergy = Math.round(monTotEnergy/10)/100
+		wkTotEnergy = Math.round(wkTotEnergy/10)/100
+	} else {
+		monAvgEnergy = Math.round(100*monAvgEnergy)/100
+		wkAvgEnergy = Math.round(100*wkAvgEnergy)/100
+		monTotEnergy = Math.round(100*monTotEnergy)/100
+		wkTotEnergy = Math.round(100*wkTotEnergy)/100
+	}
+	sendEvent(name: "monthTotalE", value: monTotEnergy)
+	sendEvent(name: "monthAvgE", value: monAvgEnergy)
+	sendEvent(name: "weekTotalE", value: wkTotEnergy)
+	sendEvent(name: "weekAvgE", value: wkAvgEnergy)
+}
+
+//	===== Obtain Week and Month Data =====
+def setCurrentDate() {
+	sendCmdtoServer('{"smartlife.iot.common.timesetting":{"get_time":null}}', "deviceCommand", "currentDateResponse")
+}
+
+def currentDateResponse(cmdResponse) {
+	def currDate =  cmdResponse["smartlife.iot.common.timesetting"]["get_time"]
+	state.dayToday = currDate.mday.toInteger()
+	state.monthToday = currDate.month.toInteger()
+	state.yearToday = currDate.year.toInteger()
+	def dateToday = Date.parse("yyyy-MM-dd", "${currDate.year}-${currDate.month}-${currDate.mday}")
+	def monStartDate = dateToday - 30
+	def wkStartDate = dateToday - 7
+	state.dayStart = monStartDate[Calendar.DAY_OF_MONTH].toInteger()
+	state.monthStart = monStartDate[Calendar.MONTH].toInteger() + 1
+	state.yearStart = monStartDate[Calendar.YEAR].toInteger()
+	state.weekStart = wkStartDate[Calendar.DAY_OF_MONTH].toInteger()
+}
+
+//	===== Send the Command to the Cloud or Bridge =====
 private sendCmdtoServer(command, hubCommand, action){
 	def appServerUrl = getDataValue("appServerUrl")
 	def deviceId = getDataValue("deviceId")
@@ -174,6 +390,18 @@ def actionDirector(action, cmdResponse) {
 	switch(action) {
 		case "commandResponse":
 			commandResponse(cmdResponse)
+			break
+		case "energyMeterResponse":
+			energyMeterResponse(cmdResponse)
+			break
+		case "useTodayResponse":
+			useTodayResponse(cmdResponse)
+			break
+		case "currentDateResponse":
+			currentDateResponse(cmdResponse)
+			break
+		case "engrStatsResponse":
+			engrStatsResponse(cmdResponse)
 			break
 		default:
 			log.info "Interface Error.  See SmartApp and Device error message."
